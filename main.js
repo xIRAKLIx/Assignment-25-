9da9aaab6ca5fbb7ee5593064bfc562efae239f3
xIRAKLIx <iraklichelidze32@gmail.com>
2025-02-06 17:00:40 +0400

    s
    s

--- main.js
+++ main.js
@@ -55,10 +55,51 @@ let sayHi1 = () => console.log("Hello");
 
 let double1 = n => n * 2;
 
-if (
-    id === 123 &&
-    moonPhase === 'Waning Gibbous' &&
-    zodiacSign === 'Libra'
-  ) {
-    letTheSorceryBegin();
-  }
+
+
+  let Jan01_1970 = new Date(0);
+console.log( Jan01_1970 );
+
+// now add 24 hours, get 02.01.1970 UTC+0
+let Jan02_1970 = new Date(24 * 3600 * 1000);
+console.log( Jan02_1970 );
+
+let Dec31_1969 = new Date(-24 * 3600 * 1000);
+console.log( Dec31_1969 );
+
+let date2 = new Date("2017-01-26");
+console.log(date2);
+
+let date3 = new Date(2011, 0, 1, 2, 3, 4, 567);
+console.log( date3 ); 
+
+let today = new Date();
+
+today.setHours(0);
+console.log(today); 
+
+today.setHours(0, 0, 0, 0);
+console.log(today); 
+
+let start = new Date(); // start measuring time
+
+// do the job
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i;
+}
+
+let end1 = new Date(); // end measuring time
+
+console.log( `The loop took ${end1 - start} ms` );
+
+
+let start1 = Date.now(); // milliseconds count from 1 Jan 1970
+
+// do the job
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i;
+}
+
+let end = Date.now(); // done
+
+console.log( `The loop took ${end - start} ms` );
